00047e650a394ff1752ec3987e1eec04497fb0a9
Diego Cordero <admin@lammworks.com>
2023-06-22 13:46:45 -0500

    testing tax and tax amount

--- payment_form.php
+++ payment_form.php
@@ -9,7 +9,7 @@
     <input type="hidden" name="access_key" value="7ab6e48404b33cc5811ccbf6657202cd">
     <input type="hidden" name="profile_id" value="833BB70A-BC54-4503-A008-90DED344993D">
     <input type="hidden" name="transaction_uuid" value="<?php echo uniqid() ?>">
-    <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,bill_to_address_line1,bill_to_address_city,bill_to_address_country,bill_to_email,bill_to_forename,bill_to_surname">
+    <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,bill_to_address_line1,bill_to_address_city,bill_to_address_country,bill_to_email,bill_to_forename,bill_to_surname,req_tax_amount,tax_indicator">
     <input type="hidden" name="unsigned_field_names">
     <input type="hidden" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>">
     <input type="hidden" name="locale" value="es">
@@ -26,6 +26,8 @@
 <span>bill_to_email:</span><input type="text" name="bill_to_email" size="25"><br/>
 <span>bill_to_forename:</span><input type="text" name="bill_to_forename" size="25"><br/>
 <span>bill_to_surname:</span><input type="text" name="bill_to_surname" size="25"><br/>
+<span>req_tax_amount:</span><input type="text" name="req_tax_amount" size="25"><br/>
+<span>tax_indicator:</span><input type="text" name="tax_indicator" size="25"><br/>
 
         </div>
     </fieldset>
