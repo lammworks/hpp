5b5e871f884d40c45fecef7103a0221adc6a7562
Diego Cordero <admin@lammworks.com>
2023-06-22 14:55:19 -0500

    changes to form so that its all burned in

--- payment_form.php
+++ payment_form.php
@@ -13,24 +13,18 @@
     <input type="hidden" name="unsigned_field_names">
     <input type="hidden" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>">
     <input type="hidden" name="locale" value="es">
-    <fieldset>
-        <legend>Payment Details</legend>
-        <div id="paymentDetailsSection" class="section">
-            <span>transaction_type:</span><input type="text" name="transaction_type" size="25"><br/>
-            <span>reference_number:</span><input type="text" name="reference_number" size="25"><br/>
-            <span>amount:</span><input type="text" name="amount" size="25"><br/>
-            <span>currency:</span><input type="text" name="currency" size="25"><br/>
-            <span>bill_to_address_line1:</span><input type="text" name="bill_to_address_line1" size="25"><br/>
-<span>bill_to_address_city:</span><input type="text" name="bill_to_address_city" size="25"><br/>
-<span>bill_to_address_country:</span><input type="text" name="bill_to_address_country" size="25"><br/>
-<span>bill_to_email:</span><input type="text" name="bill_to_email" size="25"><br/>
-<span>bill_to_forename:</span><input type="text" name="bill_to_forename" size="25"><br/>
-<span>bill_to_surname:</span><input type="text" name="bill_to_surname" size="25"><br/>
-<span>req_tax_amount:</span><input type="text" name="req_tax_amount" size="25"><br/>
+    <input type="hidden" name="transaction_type" value="sale,create_payment_token">
+    <input type="hidden" name="currency" value="USD">
+    <input type="hidden" name="bill_to_address_country" value="PA">
+    <input type="hidden" name="reference_number" value="<?php echo uniqid() ?>">
+    <input type="hidden" name="amount" value="5.50">
+    <input type="hidden" name="bill_to_address_line1" value="Calle Segunda">
+    <input type="hidden" name="bill_to_address_city" value="Panama">
+    <input type="hidden" name="bill_to_email" value="diegoc@gmail.com">
+    <input type="hidden" name="bill_to_forename" value="Diego">
+    <input type="hidden" name="bill_to_surname" value="Cordero">
+    <input type="hidden" name="req_tax_amount" value="0.50">
 
-
-        </div>
-    </fieldset>
     <input type="submit" id="submit" name="submit" value="Submit"/>
     <script type="text/javascript" src="payment_form.js"></script>
 </form>
