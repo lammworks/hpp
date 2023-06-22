8681eecab45d24c8b840148af462aaeffd762690
Diego Cordero <admin@lammworks.com>
2023-06-22 13:35:10 -0500

    added minimum fields requiered
    bill_to_address_line1
     bill_to_address_city
     bill_to_address_country
    bill_to_email
    bill_to_forename
    bill_to_surname

--- payment_form.php
+++ payment_form.php
@@ -9,10 +9,10 @@
     <input type="hidden" name="access_key" value="7ab6e48404b33cc5811ccbf6657202cd">
     <input type="hidden" name="profile_id" value="833BB70A-BC54-4503-A008-90DED344993D">
     <input type="hidden" name="transaction_uuid" value="<?php echo uniqid() ?>">
-    <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency">
+    <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,bill_to_address_line1,bill_to_address_city,bill_to_address_country,bill_to_email,bill_to_forename,bill_to_surname">
     <input type="hidden" name="unsigned_field_names">
     <input type="hidden" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>">
-    <input type="hidden" name="locale" value="en">
+    <input type="hidden" name="locale" value="es">
     <fieldset>
         <legend>Payment Details</legend>
         <div id="paymentDetailsSection" class="section">
@@ -20,6 +20,13 @@
             <span>reference_number:</span><input type="text" name="reference_number" size="25"><br/>
             <span>amount:</span><input type="text" name="amount" size="25"><br/>
             <span>currency:</span><input type="text" name="currency" size="25"><br/>
+            <span>bill_to_address_line1:</span><input type="text" name="bill_to_address_line1" size="25"><br/>
+<span>bill_to_address_city:</span><input type="text" name="bill_to_address_city" size="25"><br/>
+<span>bill_to_address_country:</span><input type="text" name="bill_to_address_country" size="25"><br/>
+<span>bill_to_email:</span><input type="text" name="bill_to_email" size="25"><br/>
+<span>bill_to_forename:</span><input type="text" name="bill_to_forename" size="25"><br/>
+<span>bill_to_surname:</span><input type="text" name="bill_to_surname" size="25"><br/>
+
         </div>
     </fieldset>
     <input type="submit" id="submit" name="submit" value="Submit"/>
