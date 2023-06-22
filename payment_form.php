4d52c86d15934df83757d238d237a547dd17b5a2
Diego Cordero <admin@lammworks.com>
2023-06-22 13:56:36 -0500

    second teak

--- payment_form.php
+++ payment_form.php
@@ -10,7 +10,7 @@
     <input type="hidden" name="profile_id" value="833BB70A-BC54-4503-A008-90DED344993D">
     <input type="hidden" name="transaction_uuid" value="<?php echo uniqid() ?>">
     <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,bill_to_address_line1,bill_to_address_city,bill_to_address_country,bill_to_email,bill_to_forename,bill_to_surname,req_tax_amount,tax_indicator">
-    <input type="hidden" name="unsigned_field_names" value="req_tax_amount,req_item_#_quantity,req_item_#_sku,req_item_#_tax_amount,req_item_#_unit_price,tax_indicator">
+    <input type="hidden" name="unsigned_field_names" value="req_tax_amount,req_item_0_quantity,req_item_0_sku,req_item_0_tax_amount,req_item_0_unit_price,tax_indicator">
     <input type="hidden" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>">
     <input type="hidden" name="locale" value="es">
     <fieldset>
@@ -27,10 +27,10 @@
 <span>bill_to_forename:</span><input type="text" name="bill_to_forename" size="25"><br/>
 <span>bill_to_surname:</span><input type="text" name="bill_to_surname" size="25"><br/>
 <span>req_tax_amount:</span><input type="text" name="req_tax_amount" size="25"><br/>
-<span>req_item_#_quantity:</span><input type="text" name="req_item_#_quantity" size="25"><br/>
-<span>req_item_#_sku:</span><input type="text" name="req_item_#_sku" size="25"><br/>
-<span>req_item_#_tax_amount:</span><input type="text" name="req_item_#_tax_amount" size="25"><br/>
-<span>req_item_#_unit_price:</span><input type="text" name="req_item_#_unit_price" size="25"><br/>
+<span>req_item_0_quantity:</span><input type="text" name="req_item_#_quantity" size="25"><br/>
+<span>req_item_0_sku:</span><input type="text" name="req_item_#_sku" size="25"><br/>
+<span>req_item_0_tax_amount:</span><input type="text" name="req_item_#_tax_amount" size="25"><br/>
+<span>req_item_0_unit_price:</span><input type="text" name="req_item_#_unit_price" size="25"><br/>
 <span>tax_indicator:</span><input type="text" name="tax_indicator" size="25"><br/>
 
         </div>
