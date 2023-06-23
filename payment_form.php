4569671bbadd079489edd4288d8596d2fcfc87f2
Diego Cordero <admin@lammworks.com>
2023-06-23 11:32:21 -0500

    Update payment_form.php
    changes to test and compare the signature, these should be deleted afterwards

--- payment_form.php
+++ payment_form.php
@@ -8,10 +8,10 @@
 <form id="payment_form" action="payment_confirmation.php" method="post">
     <input type="hidden" name="access_key" value="7ab6e48404b33cc5811ccbf6657202cd">
     <input type="hidden" name="profile_id" value="833BB70A-BC54-4503-A008-90DED344993D">
-    <input type="hidden" name="transaction_uuid" value="<?php echo uniqid() ?>">
+    <input type="hidden" name="transaction_uuid" value="6495bd0e8b4b1">
     <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,bill_to_address_line1,bill_to_address_city,bill_to_address_country,bill_to_email,bill_to_forename,bill_to_surname,req_tax_amount">
     <input type="hidden" name="unsigned_field_names">
-    <input type="hidden" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>">
+    <input type="hidden" name="signed_date_time" value="2023-06-23T15:41:02Z">
     <input type="hidden" name="locale" value="es-es">
     <input type="hidden" name="transaction_type" value="sale,create_payment_token">
     <input type="hidden" name="currency" value="USD">
