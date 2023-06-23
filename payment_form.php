a7f50b18c012b3eb56b060a7c5ac27a393a96368
Diego Cordero <admin@lammworks.com>
2023-06-23 10:40:33 -0500

    Update payment_form.php

--- payment_form.php
+++ payment_form.php
@@ -12,12 +12,12 @@
     <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,bill_to_address_line1,bill_to_address_city,bill_to_address_country,bill_to_email,bill_to_forename,bill_to_surname,req_tax_amount">
     <input type="hidden" name="unsigned_field_names">
     <input type="hidden" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>">
-    <input type="hidden" name="locale" value="es">
+    <input type="hidden" name="locale" value="es-es">
     <input type="hidden" name="transaction_type" value="sale,create_payment_token">
     <input type="hidden" name="currency" value="USD">
     <input type="hidden" name="bill_to_address_country" value="PA">
-    <input type="hidden" name="reference_number" value="<?php echo uniqid() ?>">
-    <input type="hidden" name="amount" value="5.50">
+    <input type="hidden" name="reference_number" value="1234abcd">
+    <input type="hidden" name="amount" value="100.00">
     <input type="hidden" name="bill_to_address_line1" value="Calle Segunda">
     <input type="hidden" name="bill_to_address_city" value="Panama">
     <input type="hidden" name="bill_to_email" value="diegoc@gmail.com">
